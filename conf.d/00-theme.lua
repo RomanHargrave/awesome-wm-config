5d5ca40988403c47822b76f203b8d0326ca3b9b5
Roman Hargrave <roman@hargrave.info>
2020-11-13 21:17:24 -0600

    increase theme font size

--- conf.d/00-theme.lua
+++ conf.d/00-theme.lua
@@ -7,5 +7,7 @@ local util  = require('gears')
 local theme = require('beautiful')
 
 return function(state, full)
-   theme.init(util.filesystem.get_themes_dir() .. 'zenburn/theme.lua')
+   theme.init(util.filesystem.get_themes_dir() .. 'default/theme.lua')
+   --theme.init({ font = 'sans 10' })
+   theme.font = 'sans 12'
 end
