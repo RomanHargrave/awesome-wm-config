a3b2c142f2efb0e3e2f94780c11b20e04dc8d68c
Roman Hargrave <roman@hargrave.info>
2020-11-14 22:28:39 -0600

    Add konsole to keys

--- conf.d/00-keys.lua
+++ conf.d/00-keys.lua
@@ -7,20 +7,23 @@ local wm  = require('awful')
 return function(state, full)
    state.keys = {
       -- Toggle main wibar visibility on current screen
-      wm.key({state.modkey}, "w", function()
+      wm.key({state.modkey}, 'w', function()
             local wibar = mouse.screen.wibar
             wibar.visible = not wibar.visible
       end),
 
       -- Media control
-      wm.key({}, "XF86AudioPlay", function() state.media:play_pause() end),
-      wm.key({}, "XF86AudioNext", function() state.media:next() end),
-      wm.key({"Mod1"}, "XF86AudioNext", function() state.media:previous() end),
+      wm.key({}, 'XF86AudioPlay', function() state.media:play_pause() end),
+      wm.key({}, 'XF86AudioNext', function() state.media:next() end),
+      wm.key({'Mod1'}, 'XF86AudioNext', function() state.media:previous() end),
 
       -- Activate flameshot
-      wm.key({}, "F22", function() wm.spawn("flameshot gui") end),
+      wm.key({}, 'F22', function() wm.spawn('flameshot gui') end),
 
       -- Toggle yakuake
-      wm.key({}, "F24", function() wm.spawn("yakuake") end)
+      wm.key({}, 'F24', function() wm.spawn('yakuake') end),
+
+      -- Open terminal
+      wm.key({state.modkey}, 'Return', function() awful.spawn('konsole') end),
    }
 end
